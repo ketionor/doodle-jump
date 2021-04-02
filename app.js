8a5118f6d4450a66162db25e2fb66013dfb14b94
Josh <joshua.sandefer@gmail.com>
2021-04-02 08:46:39 -0500

    collision detection on game window walls, score, new game button

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ const game = () => {
   grid.style.width = `${gridWidth}px`;
   grid.style.height = `${gridHeight}px`;
 
+  const scoreBoard = document.querySelector(".score-board");
+
   let isGameOver = false;
   let platformCount = 5;
   let platforms = [];
   let framerate = 30;
   let gameSpeed = 10;
+  let score = 0;
 
   class Doodler {
     constructor(x, y) {
       this.visual = document.createElement("div");
-      this.visual.classList.add("doodler");
+      this.visual.className = "doodler game-element";
       document.addEventListener("keydown", this.setMovement);
       document.addEventListener("keyup", this.stopMovement);
 
@@ -31,23 +34,29 @@ const game = () => {
         y: y,
       };
 
+      this.height = 85;
+      this.width = 60;
+
       this.visual.style.left = this.position.x + "px";
       this.visual.style.bottom = this.position.y + "px";
     }
 
     updatePosition(direction) {
       if (!this.gridIsMoving) {
-        this.position.y = this.position.y + this.doodlerSpeed * direction;
-        this.visual.style.bottom = this.position.y + "px";
+        this.position.y += this.doodlerSpeed * direction;
+        this.visual.style.bottom = `${this.position.y}px`;
       }
 
-      if (this.isMovingLeft === true) {
-        this.position.x = this.position.x - this.doodlerSpeed;
-        this.visual.style.left = this.position.x + "px";
+      if (this.isMovingLeft === true && this.position.x >= 0) {
+        this.position.x -= this.doodlerSpeed;
+        this.visual.style.left = `${this.position.x}px`;
       }
 
-      if (this.isMovingRight === true) {
-        this.position.x = this.position.x + this.doodlerSpeed;
+      if (
+        this.isMovingRight === true &&
+        this.position.x < gridWidth - this.width
+      ) {
+        this.position.x += this.doodlerSpeed;
         this.visual.style.left = this.position.x + "px";
       }
     }
@@ -69,10 +78,14 @@ const game = () => {
 
     gravity = () => {
       platforms.forEach((platform) => {
-        if (this.position.y > 450 && this.isJumping) {
+        if (this.position.y > gridHeight / 2 && this.isJumping) {
           let newPosition = platform.position.y - this.gridspeed;
           platform.position.y = newPosition;
           platform.bottom = newPosition + "px";
+
+          score += this.doodlerSpeed / 10;
+          scoreBoard.innerHTML = `Score: ${score}`;
+
           this.gridIsMoving = true;
         } else {
           this.gridIsMoving = false;
@@ -93,6 +106,7 @@ const game = () => {
         }
       });
 
+      //Create a new section of platforms off screen to be used when the doodler moves up
       if (platforms[platforms.length - 1].position.y < 1000) {
         createPlatforms(platforms[platforms.length - 1].position.y);
       }
@@ -106,7 +120,7 @@ const game = () => {
       }
 
       if (this.position.y <= 0) {
-        alert("Game over!");
+        console.log("Game over!");
       } else {
         requestAnimationFrame(this.gravity);
       }
@@ -116,7 +130,7 @@ const game = () => {
   class Platform {
     constructor(newPlatBottom) {
       this.visual = document.createElement("div");
-      this.visual.classList.add("platform");
+      this.visual.className = "platform";
       this.width = 85;
       this.height = 15;
 
@@ -138,7 +152,8 @@ const game = () => {
 
   const createPlatforms = (minY) => {
     const platformSection = document.createElement("div");
-    platformSection.classList.add("platformSection");
+    platformSection.className = "platformSection game-element";
+
     platformSection.style.width = `${gridWidth}px`;
     platformSection.style.height = `${gridHeight}px`;
     grid.insertBefore(platformSection, grid.firstChild);
@@ -148,7 +163,9 @@ const game = () => {
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1) + min);
     };
+
     let platformCount = getRand(5, 15);
+
     for (let i = 0; i < platformCount; i++) {
       let platGap = gridHeight / platformCount;
       let newPlat = new Platform(minY + i * platGap);
@@ -162,6 +179,13 @@ const game = () => {
   };
 
   const startGame = () => {
+    //Remove any existing game elements
+    const gameElement = document.querySelectorAll(".game-element");
+    gameElement.forEach((element) => {
+      console.log("removing: ", element);
+      grid.removeChild(element);
+    });
+    platforms = [];
     createPlatforms(100);
     createPlatforms(gridHeight);
     let doodler = new Doodler(
@@ -174,6 +198,10 @@ const game = () => {
   };
 
   startGame();
+
+  //New game button
+  const newGameButton = document.querySelector(".new-game-button");
+  newGameButton.addEventListener("click", startGame);
 };
 
 game();

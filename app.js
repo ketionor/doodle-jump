609c989a7f92dddd3603bbe1f6e1993be8b26f5d
Josh <joshua.sandefer@gmail.com>
2021-04-13 16:02:39 -0500

    deploy

---
+++ app.js
@@ -0,0 +1,16 @@
+// create an express app
+const express = require("express");
+const { url } = require("node:inspector");
+const app = express();
+var path = require("path");
+
+// use the express-static middleware
+app.use(express.static("public"));
+
+// define the first route
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
